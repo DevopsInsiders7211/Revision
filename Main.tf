053d7def2109d7f895a5b7a3071a1db7506cd751
Rishikesh Sharma <srishikesh39@gmai.com>
2025-10-03 14:02:40 +0530

    RG added

--- Main.tf
+++ Main.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.27.0"
+      version = "4.41.0"
     }
   }
   backend "azurerm" {
-    resource_group_name  = "Backendrg"
-    storage_account_name = "storageaccount7211"
-    container_name       = "rishicontainer"
+    resource_group_name  = "App_RG"
+    storage_account_name = "appstg"
+    container_name       = "appcontainer"
     key                  = "terraform.tfstate"
   }
 
 }
 provider "azurerm" {
   features {}
-  subscription_id = "c7389b7c-212f-4f63-a992-172d719df867"
+  subscription_id = "88fc7642-e677-4cd7-b276-4d2f8cc6ad1d"
 }
 resource "azurerm_resource_group" "motorg" {
   name     = "MotoRG"
